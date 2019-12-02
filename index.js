1f1689122812bff28d9b9a210eb113f5e1d2dd89
Aurel37 <pion.aurelien@outlook.com>
2019-12-02 13:50:34 +0100

    connection a la base de donnee, premieres insertions de donnees

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var fs = require('fs')
-var vm = require('vm')
 var express = require('express');
 var session = require('cookie-session');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-var model = require('./model')
+var model = require('./model');
+var url = require('url');
 
 model.db_manager;
 
@@ -13,14 +12,26 @@ var app = express();
 app.use(session({secret: 'shotgun'}))
 
 .use('shotgun' , function(req, res, next) {
-	if (typeof(req.session.answers) == 'undefined') {
-		req.session.answers = [];
-	}		
+		
 	next();
 })
 
 .get('/shotgun', function(req, res) {
 	res.render('main_page.ejs', {answers: req.session.answers});
+})
+
+.post('/shotgun/add/year', urlencodedParser, function(req, res) {
+	if (typeof(req.session.answers) == 'undefined') {
+		req.session.answers = [];
+	}	
+	model.add_data(req.body.year, 'year', 'student');
+	res.redirect('/shotgun');
+	
+})
+
+.post('/shotgun/add/year', urlencodedParser, function(req, res) {
+	model.add_data(req.body.class1, 'class1', 'student');
+	res.redirect('/shotgun');
 })
 
 .use(function(req, res, next){
--- model.js
+++ model.js
@@ -4,7 +4,8 @@ var con = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
 	password : 'root',
-	socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock'
+	socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock',
+	database : 'shotgun_website'
 });
 
 con.connect(function(err) {
@@ -12,7 +13,27 @@ con.connect(function(err) {
 	console.log('connected !');
 });
 
+function add_data(data, column, table, first) {
+	if (data != '') 
+	{
+		if (first) {
+			var sql = 'INSERT INTO ' +  table + ' (' + column + ') '  +  "VALUES" + ' (' + data + ')';
+		}
+		else
+		{
+			var sql = 'INSERT INTO ' +  table + ' (' + column + ') '  +  "VALUES" + ' (' + data + ')';
+		}
+		con.query(sql, function (err, result) {
+			if  (err) throw err;
+			console.log("1 record inserted");
+		});
+	}
+
+};
+
+
 exports.db_manager = con;
+exports.add_data = add_data;
 
 
 
