08b04ea0a5a4c224e8d5fc5ccab2b6ee4446fe56
Rouraf <44207374+Rouraf@users.noreply.github.com>
2020-01-25 17:04:04 +0100

    Add files via upload

--- optimization2.py
+++ optimization2.py
@@ -10,25 +10,35 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   passwd="root",
-  db="site"
+  db="essai"
 )
 
 ## Constante
-nb_choix=4
 coef_choix=3
+
+imposs = 1000
 
 ## Constante prise à partir de la base de donnée
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM students")
 myresult = mycursor.fetchall()
 students=[]
+nb_hours=[]
+language_wanted=[]
 for x in myresult:
-    (id, nom, sport, prom)=x
-    students.append([id,nom,sport,prom])
+    (id, nom, prenom, sport, prom, hour_asked)=x
+    students.append([id-1,nom, prenom,sport,prom,hour_asked])
+    nb_hours.append(hour_asked)
+    language_wanted.append([])
 nb_students = len(students)
 
+mycursor.execute("SELECT * FROM language_wanted")
+myresult=mycursor.fetchall
+for x in mycursor:
+    (id, id_student, id_language)=x
+    language_wanted[id_student-1].append(id_language)
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM créneau")
+mycursor.execute("SELECT * FROM slots")
 myresult = mycursor.fetchall()
 slots_courses=[]
 for x in myresult:
@@ -39,6 +49,7 @@ mycursor.execute("SELECT * FROM cours")
 myresult = mycursor.fetchall()
 courses=[]
 capacities=[]
+
 for x in myresult:
     (id_cours, id_creneau, id_langue, niveau, nom, enseignant, effectif)=x
     courses.append([id_cours, id_creneau, id_langue, niveau, nom])
@@ -48,41 +59,79 @@ for x in myresult:
 nb_courses = len(courses)
 nb_slots = len(slots_courses)
 
-choix=[]
+choices=[]
 
-for i in range(nb_students):
-    for k in range(1,5):
-        choix.append([i,np.random.randint(nb_courses),k])
+mycursor.execute("SELECT * FROM choices")
+myresult= mycursor.fetchall()
+for x in myresult:
+    (id, id_student, id_class, rank)=x
+    choices.append([id_student-1, id_class, rank])
+
         
-        
+mycursor.execute("SELECT * FROM sports")
+myresult= mycursor.fetchall()
+sports=[]
+for x in myresult:
+    (id, name, id_slot)=x
+    sports.append(id_slot)
 
 
-imposs = 1000
+# Which year can choose wich course?
+mycursor.execute("SELECT * FROM for_who")
+myresult= mycursor.fetchall()
+for_who=[[],[],[]]
+for x in myresult:
+    (id, id_slot, id_year)=x
+    for_who[id_year].append(id_slot)
 
-## Variable vectors (fill in with the students' requests)
+def wrong_year(i, j):
+    student_year=students[i][4]
+    for slot in for_who[student_year]:
+        if slot==courses[j][1]:
+            return False
+    return True
+
+def wrong_language(i, j):
+    wrong=True
+    language_course=courses[j][2]
+    for language in language_wanted[i]:
+        if language==language_course:
+            wrong=False
+    return wrong
+
 
 # list of weights given by each students to each courses
 def create_weight(choices):
     #Create table
-    a=int(coef_choix*nb_choix)
+    a=100
     weights=np.ones((nb_students, nb_courses))*a
     
-    #Order choice 
+    # Courses chosed 
     for choice in choices:
         student=choice[0]
         course=choice[1]
         rank=choice[2]
         weights[student, course]=(rank-1)*coef_choix
 
+    # Impossible courses
+    for j in range(nb_students):
+        for i in range(nb_courses):
+            sport=students[j][3]
+            if courses[i][1]==sports[sport]:
+                weights[j, i] = imposs
+            if wrong_year(j,i):
+                weights[j, i] = imposs
+            if wrong_language(j,i):
+                weights[j,i] = imposs
+
     
     return weights
 
-weight=create_weight(choix)
+weight=create_weight(choices)
+
     
 
 
-# list of number of lessons wanted for each students
-nb_hours = np.ones(nb_students)
 
 ## Optimization
 
@@ -111,12 +160,14 @@ prob.solve()
 
 # display the results
 for student in range(nb_students):
-    string_row = "student " + students[student][1] + " get courses"
+    string_row = "student " + students[student][1] + " " + students[student][2] + " get courses"
     for course in range(nb_courses):
         if int(allocation[(student,course)].value()) == 1:
-            string_row += " " + courses[course][4]
+            string_row += " " + courses[course][4] + ","
     print(string_row)
 
+print("Dissatisfaction per student =",  prob.objective.value()/nb_students)
+#print("choix =", choix)
 # store and display the results
 # with open("allocation.txt",'w') as f:
 #     for student in range(nb_students):
