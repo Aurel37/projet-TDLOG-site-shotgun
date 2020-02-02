24e394ddcd1e424ae65dfd4e7f80569eb794cb40
Rouraf <44207374+Rouraf@users.noreply.github.com>
2020-02-02 17:17:28 +0100

    Add files via upload

--- optimization.py
+++ optimization.py
@@ -16,63 +16,67 @@ config = {
 
 
 mydb = mysql.connector.connect(**config)
+
 ## Constant
 
 # coefficient associated with the rank of choice
-choice_coefficient=3
+choice_coefficient = 3
 
-#weight of an impossible choice
-impossible_choice= 10000
+# weight of an impossible choice
+impossible_choice = 10000
 
 ## Constant taken from the database
 
-#From table students
+# From table students
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM students")
 myresult = mycursor.fetchall()
-#information about each student
-students=[]
-#number of hours each student want
-number_hours=[]
-#language each student want to learn
-language_wanted=[]
+# information about each student
+students = []
+# number of hours each student want
+number_hours = []
+# language each student want to learn
+language_wanted = []
 for x in myresult:
-    (id, surname, name, sport, year, hour_asked)=x
-    students.append([id-1,surname, name,sport,year])
+    (id, surname, name, sport, year, hour_asked) = x
+    students.append([id-1, surname, name, sport, year])
     number_hours.append(hour_asked)
     language_wanted.append([])
 nb_students = len(students)
 
-#From language_wanted
+# From language_wanted
 mycursor.execute("SELECT * FROM language_wanted")
-myresult=mycursor.fetchall
+myresult = mycursor.fetchall
 for x in mycursor:
-    (id, id_student, id_language)=x
-    language_wanted[id_student-1].append(id_language)
+    (id, id_student, id_language) = x
+    language_wanted[id_student - 1].append(id_language)
 mycursor = mydb.cursor()
 
-#From slots
+# From slots
 mycursor.execute("SELECT * FROM slots")
 myresult = mycursor.fetchall()
-#slots_cources tells us what courses take place during a given hour
-slots_courses=[]
+# slots_cources tells us what courses take place during a given hour
+slots_courses = []
 for x in myresult:
     slots_courses.append([])
 
-#From cours
+print(students)
+
+# From cours
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM cours")
 myresult = mycursor.fetchall()
-#information about each course
-courses=[]
-#capacitie of each course
-capacities=[]
+# information about each course
+courses = []
+# capacitie of each course
+capacities = []
 
 # table of courses gathered by language
-language_courses = [[] for _  in range(10)]
+language_courses = [[] for i in range(10)]
 
 for x in myresult:
-    (id_cours, id_creneau, id_langue, level, name, capacity)=x
+    print(x)
+    (id_cours, id_creneau, id_langue, level, name, teacher, capacity) = x
     courses.append([id_cours, id_creneau, id_langue, level, name])
     capacities.append(capacity)
     language_courses[id_langue].append(id_cours)
@@ -80,54 +84,55 @@ for x in myresult:
 
 number_courses = len(courses)
 
-#From choices
-#contains each choice of each student
-choices=[]
+# From choices
+# contains each choice of each student
+choices = []
 
 mycursor.execute("SELECT * FROM choices")
-myresult= mycursor.fetchall()
+myresult = mycursor.fetchall()
 for x in myresult:
-    (id, id_student, id_class, rank)=x
+    (id, id_student, id_class, rank) = x
     choices.append([id_student-1, id_class, rank])
 
-#From sports
+# From sports
 mycursor.execute("SELECT * FROM sports")
-myresult= mycursor.fetchall()
-#hour of each sport
-sports=[]
+myresult = mycursor.fetchall()
+# hour of each sport
+sports = []
 for x in myresult:
-    (id, name, id_slot)=x
+    (id, name, id_slot) = x
     sports.append(id_slot)
 
 
 # Which year can choose wich course?
 mycursor.execute("SELECT * FROM for_who")
-myresult= mycursor.fetchall()
-for_who=[[],[],[]]
+myresult = mycursor.fetchall()
+for_who = [[], [], []]
 for x in myresult:
-    (id, id_slot, id_year)=x
+    (id, id_slot, id_year) = x
     for_who[id_year].append(id_slot)
 
 
 def wrong_year(i, j):
     """"
-    return true if a course j is not for a student's i year 
+    return true if a course j is not for a student's i year
     """
-    student_year=students[i][4]
+    student_year = students[i][4]
     for slot in for_who[student_year]:
-        if slot==courses[j][1]:
+        if slot == courses[j][1]:
             return False
     return True
+
 
 def wrong_language(i, j):
     """
     Return true if a course j is not of a language a student i wants
     """
-    wrong=True
-    language_course=courses[j][2]
+    wrong = True
+    language_course = courses[j][2]
     for language in language_wanted[i]:
-        if language==language_course:
-            wrong=False
+        if language == language_course:
+            wrong = False
     return wrong
 
 
@@ -136,35 +141,30 @@ def create_weight(choices):
     """
     Create a table of weight according to the students' choices
     """
-    #Create table
-    a=100
-    weights=np.ones((nb_students, number_courses))*a
+    # Create table
+    a = 100
+    weights = np.ones((nb_students, number_courses)) * a
 
-    # Courses chosed 
+    # Courses chosed
     for choice in choices:
-        student=choice[0]
-        course=choice[1]
-        rank=choice[2]
-        weights[student, course]=(rank-1)*choice_coefficient
+        student = choice[0]
+        course = choice[1]
+        rank = choice[2]
+        weights[student, course] = (rank-1)*choice_coefficient
 
     # Select impossible courses and give them a weight=impossible_choice
     for j in range(nb_students):
         for i in range(number_courses):
-            sport=students[j][3]
-            if courses[i][1]==sports[sport]:
-                weights[j, i] = impossible_choice 
-            if wrong_year(j,i):
-                weights[j, i] = impossible_choice 
-            if wrong_language(j,i):
-                weights[j,i] = impossible_choice 
-
-
+            sport = students[j][3]
+            if courses[i][1] == sports[sport]:
+                weights[j, i] = impossible_choice
+            if wrong_year(j, i):
+                weights[j, i] = impossible_choice
+            if wrong_language(j, i):
+                weights[j, i] = impossible_choice
     return weights
 
-weight=create_weight(choices)
-
-
-
+weight = create_weight(choices)
 
 ## Optimization
 
@@ -175,22 +175,22 @@ prob = LpProblem("Minimizing dissatisfaction", LpMinimize)
 allocation = LpVariable.dicts("Allocation of the courses", ((i, j) for i in range(nb_students) for j in range(number_courses)), lowBound=0, upBound=1, cat='Integer')
 
 # objective function
-prob += lpSum([weight[i,j]*allocation[(i,j)] for i in range(nb_students) for j in range(number_courses)])
+prob += lpSum([weight[i, j]*allocation[(i, j)] for i in range(nb_students) for j in range(number_courses)])
 
 # constraints
 for i in range(nb_students):
     # each student must have the number of lessons he asked for
-    prob += lpSum([allocation[(i,j)] for j in range(number_courses)]) == number_hours[i]
+    prob += lpSum([allocation[(i, j)] for j in range(number_courses)]) == number_hours[i]
     # each student must have at least one course of english
-    prob += lpSum([allocation[(i,j)] for j in language_courses[0]]) >= 1
+    prob += lpSum([allocation[(i, j)] for j in language_courses[0]]) >= 1
     # each student must have at least one course of another language
-    prob += lpSum([allocation[(i,j)] for j in language_courses[0]]) <= number_hours[i] - 1
+    prob += lpSum([allocation[(i, j)] for j in language_courses[0]]) <= number_hours[i] - 1
     # each student must have at the most 1 lesson per slot
     for slot in slots_courses:
-        prob += lpSum([allocation[(i,j)] for j in slot]) <= 1
+        prob += lpSum([allocation[(i, j)] for j in slot]) <= 1
 for j in range(number_courses):
     # a course cannot have more than the number a student he can host
-    prob += lpSum([allocation[(i,j)] for i in range(nb_students)]) <= capacities[j]
+    prob += lpSum([allocation[(i, j)] for i in range(nb_students)]) <= capacities[j]
 
 # solve the problem
 prob.solve()
@@ -199,20 +199,20 @@ prob.solve()
 for student in range(nb_students):
     string_row = "student " + students[student][1] + " " + students[student][2] + " get courses"
     for course in range(number_courses):
-        if int(allocation[(student,course)].value()) == 1:
+        if int(allocation[(student, course)].value()) == 1:
             string_row += " " + courses[course][4] + ","
     print(string_row)
 
 print("Dissatisfaction per student =",  prob.objective.value()/nb_students/choice_coefficient)
-#print("choix =", choix)
-#store and display the results
-with open("allocation.txt",'w') as f:
-     for student in range(nb_students):
+# print("choix =", choix)
+# store and display the results
+with open("allocation.txt", 'w') as f:
+    for student in range(nb_students):
         string_row = "student " + students[student][1] + " " + students[student][2] + " get courses"
         for course in range(number_courses):
-            if int(allocation[(student,course)].value()) == 1:
+            if int(allocation[(student, course)].value()) == 1:
                 string_row += " " + courses[course][4] + ","
-         # store
+        # store
         string_row += "\n"
         f.write(string_row)
-         # display
+        # display
